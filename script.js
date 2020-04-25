bea48096b14fb7db47de9c51182b6d555633718b
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-25 19:24:47 +0800

    change everything back

--- script.js
+++ script.js
@@ -12,18 +12,6 @@ $(function() {
 	var gameOver = false;
 	var con = $('.congratulation');
 
-	function scoreAdder(pScore, pDisplay) {
-		if (!gameOver) {
-			pScore++;
-			pDisplay = pDisplay.text(pScore);
-			if (pScore === winningScore) {
-				pDisplay.addClass('winner');
-				gameOver = !gameOver;
-				animatedBounce(pDisplay);
-			}
-		}
-	}
-
 	p1Btn.on('click', function() {
 		if (!gameOver) {
 			p1Score++;
